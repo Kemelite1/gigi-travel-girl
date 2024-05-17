cce94ad8918613176501b5e7e7c89899ab1f55e3
kemelite1 <osikemelite42@gmail.com>
2024-05-17 14:03:16 +0100

    - Add media queries for responsiveness - Add javascript for the hamburger menu - Add javascript for the form modal

--- js/script.js
+++ js/script.js
@@ -3,15 +3,25 @@ let searchBar = document.querySelector('.search-bar-container');
 let formButton = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form');
 let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbarMenu = document.querySelector('.navbar-menu');
 
 window.onscroll = () => {
     searchButton.classList.remove('fa-times');
     searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbarMenu.classList.remove('active');
+    loginForm.classList.remove('active');
 };
 
 searchButton.addEventListener('click', () => {
     searchButton.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbarMenu.classList.toggle('active');
 });
 
 formButton.addEventListener('click', () => {
